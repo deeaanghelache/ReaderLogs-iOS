5a0e1fe7a1aa2d7c8e8acb5f61976be01031749e
deeaanghelache <andreea.anghelache@s.unibuc.ro>
2023-04-01 10:30:31 +0300

    Initial Setup

---
+++ ReaderLogs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReaderLogs
+//
+//  Created by Andreea Anghelache on 01.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
