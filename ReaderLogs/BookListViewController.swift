5f1bf01bf2b9876ed073417fc2f7e8e2db12f7c0
deeaanghelache <andreea.anghelache@s.unibuc.ro>
2023-04-02 17:54:59 +0300

    Menu

---
+++ ReaderLogs/BookListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BookListViewController.swift
+//  ReaderLogs
+//
+//  Created by Andreea Anghelache on 02.04.2023.
+//
+
+import UIKit
+
+class BookListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Book List"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ReaderLogs/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  ReaderLogs
+//
+//  Created by Andreea Anghelache on 02.04.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Profile"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
