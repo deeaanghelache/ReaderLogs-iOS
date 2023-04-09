47d859f26d7991d05072bfa2e48deeaedbd6af1a
deeaanghelache <andreea.anghelache@s.unibuc.ro>
2023-04-09 12:36:40 +0300

    Added Search View Controller

---
+++ ReaderLogs/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  ReaderLogs
+//
+//  Created by Andreea Anghelache on 09.04.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Search"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
