64622f20663dc4d9bd4324ebb3154709f56ed21f
deeaanghelache <andreea.anghelache@s.unibuc.ro>
2023-04-09 14:36:59 +0300

    Search bar text + hide/unhide table

--- ReaderLogs/SearchViewController.swift
+++ ReaderLogs/SearchViewController.swift
@@ -8,22 +8,109 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    // MARK: Variables
+    let searchBarView = UISearchBar()
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Search"
+        
+        // MARK: Search Bar
+        searchBarView.placeholder = "Search book by title..."
+        searchBarView.searchBarStyle = .minimal
+        searchBarView.translatesAutoresizingMaskIntoConstraints = false
+        searchBarView.delegate = self
+        view.addSubview(searchBarView)
+        
+        // MARK: Book Table
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 140.0
+        tableView.register(BookCellTableView.self, forCellReuseIdentifier: BookCellTableView.bookCellTableView)
+        tableView.backgroundColor = UIColor.white
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.isHidden = true
+        view.addSubview(tableView)
+        
+        let leadingConstraintTable = NSLayoutConstraint(item: tableView,
+                                                   attribute: .leading,
+                                                   relatedBy: .equal,
+                                                   toItem: view,
+                                                   attribute: .leading,
+                                                   multiplier: 1.0,
+                                                   constant: 10.0)
+        
+        let trailingConstraintTable = NSLayoutConstraint(item: tableView,
+                                                    attribute: .trailing,
+                                                    relatedBy: .equal,
+                                                    toItem: view,
+                                                    attribute: .trailing,
+                                                    multiplier: 1.0,
+                                                    constant: -10.0)
+        let topConstraintTable = NSLayoutConstraint(item: tableView,
+                                               attribute: .top,
+                                               relatedBy: .equal,
+                                               toItem: view.safeAreaLayoutGuide,
+                                               attribute: .top,
+                                               multiplier: 1.0,
+                                               constant: 50.0)
+        let bottomConstraintTable = NSLayoutConstraint(item: tableView,
+                                                  attribute: .bottom,
+                                                  relatedBy: .equal,
+                                                  toItem: view.safeAreaLayoutGuide,
+                                                  attribute: .bottom,
+                                                  multiplier: 1.0,
+                                                  constant: 0.0)
+        NSLayoutConstraint.activate([leadingConstraintTable,
+                                     trailingConstraintTable,
+                                     topConstraintTable,
+                                     bottomConstraintTable])
+        
+        
+        // MARK: Constraints
+        let constraints = [
+            searchBarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10.0),
+            searchBarView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10.0),
+            searchBarView.topAnchor.constraint(equalTo: view.topAnchor, constant: 15.0),
+            searchBarView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -620.0)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if (searchText != "") {
+            // functia de api
+            tableView.isHidden = false
+        } else {
+            tableView.isHidden = true
+        }
     }
-    */
+}
 
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+//        return userBooks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: BookCellTableView.bookCellTableView,
+                                                            for: indexPath) as? BookCellTableView {
+                    return cell
+                } else {
+                    return UITableViewCell()
+            }
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // aici cod pt details
+    }
 }
